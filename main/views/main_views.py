00e0695ff7144d4a6481a34f0b33c13f0d1b6419
SDM980119 <emraud119@naver.com>
2025-09-29 17:58:05 +0900

    0929_1758_SDM

--- main/views/main_views.py
+++ main/views/main_views.py
@@ -1,9 +1,9 @@
 # url 설정
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, render_template
 
 # 블루 프린트 설정
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
-    return '시작이다~'
+    return redirect(url_for('items._main'))  # 템플릿을 직접 렌더링
---
+++ main/views/review_views.py
@@ -0,0 +1,2 @@
+from flask import Blueprint, redirect, url_for
+bp = Blueprint('review', __name__, url_prefix='/review')
