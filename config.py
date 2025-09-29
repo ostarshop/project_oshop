861b2ca2c8f79bb02ef5c6fbc98be6d6656b3341
SDM <emraud119@naver.com>
2025-09-29 22:46:14 +0900

    0929_2246

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'oshop.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+SECRET_KEY = 'dev'
