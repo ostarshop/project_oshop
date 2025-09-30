2fa2a5712f9f7fc246106fa5e2c09042cee3681e
SDM <emraud119@naver.com>
2025-09-30 22:57:45 +0900

    0930_2256_SDM

--- main/views/auth_views.py
+++ main/views/auth_views.py
@@ -1,3 +1,5 @@
+import functools
+
 from flask import Blueprint, redirect, url_for, request, flash, render_template, session,g
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -48,6 +50,7 @@ def login():
         flash(error)
     return render_template('auth/login.html', form=form) # GET 방식으로 로그인 요청시 html 문서가 나오도록
 
+# 로그인한 사용자면 g.user에 유저 정보를 받아서 저장
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
@@ -56,11 +59,13 @@ def load_logged_in_user():
     else:
         g.user = User.query.get(int(user_id))
 
+# 로그아웃시 session을 삭제하고 메인 으로 보냄
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('main.index'))
 
+# 특정 페이지에 로그인한 유저가 아니면 로그인페이지로 보냄
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(*args ,**kwargs):
