491f87af4b0c38dc10b7fd50d597046e26699784
SDM980119 <emraud119@naver.com>
2025-10-01 18:01:30 +0900

    1001_1801_SDM2

--- main/views/items_views.py
+++ main/views/items_views.py
@@ -6,3 +6,7 @@ bp = Blueprint('items', __name__, url_prefix='/items')
 @bp.route('/main/')
 def _main():
     return render_template('items/items_main.html')
+
+@bp.route('/list/')
+def _list():
+    return render_template('items/items_list.html')
