1a5faff339cf2d495382f4896232d2c5f3cb1573
SDM980119 <emraud119@naver.com>
2025-10-14 16:25:52 +0900

    1014_1625_SDM

--- main/static/main_js/main_slide.js
+++ main/static/main_js/main_slide.js
@@ -1,3 +1,26 @@
+//-------------------------------------------------------------------------
+// 맨 위로 올라가는 스크롤 버튼 - JS
+//-------------------------------------------------------------------------
+
+window.addEventListener("scroll", () => {
+    const btn = document.getElementById("back-to-top");
+    if (window.scrollY > 100) {
+        btn.classList.add("visible");
+    } else {
+        btn.classList.remove("visible");
+    }
+    });
+
+    // 버튼 클릭하면 맨 위로 부드럽게 이동.
+    document.getElementById("back-to-top").addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+    });
+
+
 
 /* --------------------------------------------------------------- */
 /* Main Slider */
--- main/static/sub_js/filter.js
+++ main/static/sub_js/filter.js
@@ -6,3 +6,26 @@ document.querySelectorAll(".filter-toggle").forEach(button => {
 });
 
 
+//-------------------------------------------------------------------------
+// 맨 위로 올라가는 스크롤 버튼 - JS
+//-------------------------------------------------------------------------
+
+window.addEventListener("scroll", () => {
+    const btn = document.getElementById("back-to-top");
+    if (window.scrollY > 100) {
+        btn.classList.add("visible");
+    } else {
+        btn.classList.remove("visible");
+    }
+    });
+
+    // 버튼 클릭하면 맨 위로 부드럽게 이동.
+    document.getElementById("back-to-top").addEventListener("click", (e) => {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+    });
+
+
